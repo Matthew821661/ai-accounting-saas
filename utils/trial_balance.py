fa89f39c7e443b4a158c563a548a402e87982949
Mattrade <matthewlestrade@gmail.com>
2025-07-09 22:30:44 +0200

    Delete utils directory

--- utils/trial_balance.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-def generate_trial_balance(df):
-    df['Debit'] = df['Amount'].apply(lambda x: x if x > 0 else 0)
-    df['Credit'] = df['Amount'].apply(lambda x: -x if x < 0 else 0)
-    return df.groupby('GL Account')[['Debit', 'Credit']].sum().reset_index()
